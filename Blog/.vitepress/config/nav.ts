f76e0699c656f95f413ef3c57d513712cac3bc6a
j33h22 <ju33huang22@gmail.com>
2023-08-26 17:07:47 +0800

    修复顶栏上的关于按钮链接

--- Blog/.vitepress/config/nav.ts
+++ Blog/.vitepress/config/nav.ts
@@ -2,13 +2,13 @@ import { DefaultTheme } from "vitepress";
 
 const navConfig: DefaultTheme.NavItem[] = [
   { text: "主页", link: "/" },
-  { text: "关于", link: "/About/" },
-  //{ 
-  //  text: "了解本站",
-  //  items:[
-  //    {text: "关于，我", link: "/About/" },
-  //  ]
-  //},
+  { 
+    text: "关于",
+    items:[
+      {text: "关于，我", link: "/About/Me" },
+      { text: "关于这个站", link: "/About/About-This-Blog" },
+    ]
+  },
 ];
 
 export default navConfig;
